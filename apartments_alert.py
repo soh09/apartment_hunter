a132690958a8e5ed452cd8f0859ed46bc1e362b7
So Hirota <hirotaso92602@gmail.com>
2022-05-03 17:40:38 -0700

    made changes

--- apartments_alert.py
+++ apartments_alert.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 lux = 'https://www.on-site.com/web/online_app/choose_unit?goal=6&attr=x20&property_id=225322&lease_id=0&unit_id=0&required='
 three_60 = 'https://www.on-site.com/web/online_app/choose_unit?goal=6&attr=x20&property_id=191247&lease_id=0&unit_id=0&required='
+cv = 'https://www.on-site.com/web/online_app/choose_unit?goal=6&attr=x20&property_id=1166&lease_id=0&unit_id=0&required='
 
 def get_html(url):
     session = HTMLSession()
@@ -26,58 +27,104 @@ def make_dict(ps):
             d[current_plan].append(p.text)
     return d
 
-def get_difference(name, d_before, d_current):
+def get_difference(name, d_before, d_current, url):
     message = ""
     if (len(d_before)) > len(d_current):
         taken = ", ".join(d_before.keys() - d_current.keys())
         message = "[" + name + "] " + "Listing removed update: " + taken
     else:
         added = ", ".join(d_current.keys() - d_before.keys())
-        message = "[" + name + "] " + "New listing update: " + added
+        message = "[" + name + "] " + "New listing update: " + added + " [Website]: " + url
 
     return message
 
-def send_sms(message):
+def send_so_sms(message):
     account_sid = 'ACb89be4618d01504371ee5a71581e0a4d' 
     auth_token = '257e09038f86713f5d78f6e025a0ebdb' 
     client = Client(account_sid, auth_token) 
-    print("sending message")
-    
-    # sending to so
-    message = client.messages.create(messaging_service_sid='MG9b262dfc07ca251f557ea5206000f0cc', body=message, to='+16502914624') 
-    # sending to zack
-    message = client.messages.create(messaging_service_sid='MG9b262dfc07ca251f557ea5206000f0cc', body=message, to='+17605045501') 
-    # sending to peter
-    message = client.messages.create(messaging_service_sid='MG9b262dfc07ca251f557ea5206000f0cc', body=message, to='+16193175276') 
-    
+    message = client.messages.create(messaging_service_sid='MG9b262dfc07ca251f557ea5206000f0cc', body = message, to='16502914624')
+
+def send_zack_sms(message):
+    account_sid = 'ACb89be4618d01504371ee5a71581e0a4d' 
+    auth_token = '257e09038f86713f5d78f6e025a0ebdb' 
+    client = Client(account_sid, auth_token) 
+    message = client.messages.create(messaging_service_sid='MG9b262dfc07ca251f557ea5206000f0cc', body=message, to='17605045501')
+
+def send_peter_sms(message):
+    account_sid = 'ACb89be4618d01504371ee5a71581e0a4d' 
+    auth_token = '257e09038f86713f5d78f6e025a0ebdb' 
+    client = Client(account_sid, auth_token) 
+    message = client.messages.create(messaging_service_sid='MG9b262dfc07ca251f557ea5206000f0cc', body=message, to='16193175276')
+
+def send_sam_sms(message):
+    account_sid = 'ACb89be4618d01504371ee5a71581e0a4d' 
+    auth_token = '257e09038f86713f5d78f6e025a0ebdb'
+    client = Client(account_sid, auth_token) 
+    message = client.messages.create(messaging_service_sid='MG9b262dfc07ca251f557ea5206000f0cc', body=message, to='16266861588')
+
+def send_adrian_sms(message):
+    account_sid = 'ACb89be4618d01504371ee5a71581e0a4d' 
+    auth_token = '257e09038f86713f5d78f6e025a0ebdb'
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(messaging_service_sid='MG9b262dfc07ca251f557ea5206000f0cc', body=message, to='18476248866')
+
+def send_dang_sms(message):
+    account_sid = 'ACb89be4618d01504371ee5a71581e0a4d' 
+    auth_token = '257e09038f86713f5d78f6e025a0ebdb'
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(messaging_service_sid='MG9b262dfc07ca251f557ea5206000f0cc', body=message, to='16692437630') 
+
+  
 
 minute_before_lux_d = make_dict(get_html(lux))
 minute_before_three_60_d = make_dict(get_html(three_60))
+minute_before_cv_d = make_dict(get_html(cv))
 
 
 while (True):
     three_60_d = make_dict(get_html(three_60))
     lux_d = make_dict(get_html(lux))
+    cv_d = make_dict(get_html(cv))
 
     if len(minute_before_lux_d) != len(lux_d):
-        message = get_difference("Lux UTC", minute_before_lux_d, lux_d)
+        message = get_difference("Lux UTC", minute_before_lux_d, lux_d, lux)
         minute_before_lux_d = lux_d
         print("sending update: " + message)
-        send_sms(message)
+        send_so_sms(message)
+        send_zack_sms(message)
+        # send_sam_sms(message, lux)
+        send_peter_sms(message)
+        send_adrian_sms(message)
 
     if len(minute_before_three_60_d) != len(three_60_d):
-        message = get_difference("360 Lux", minute_before_three_60_d, three_60_d)
+        message = get_difference("360", minute_before_three_60_d, three_60_d, three_60)
         minute_before_three_60_d = three_60_d
         print("sending update: " + message)
-        send_sms(message)
+        send_so_sms(message)
+        send_zack_sms(message)
+        # send_sam_sms(message, lux)
+        send_peter_sms(message)
+        send_adrian_sms(message)
+    
+    if len(minute_before_cv_d) != len(cv_d):
+        message = get_difference("Costa Verde", minute_before_cv_d, cv_d, cv)
+        minute_before_cv_d = cv_d
+        print("sending update: " + message)
+        send_so_sms(message)
+        send_zack_sms(message)
+        # send_sam_sms(message, lux)
+        send_peter_sms(message)
+        send_adrian_sms(message)
+        send_dang_sms(message)
 
     now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
     
     print("--------------------------------------")
     print("Current status at " + dt_string+ ": ")
     print("Lux UTC: " + ", ".join(lux_d.keys()))
-    print("360 Luxury: " + ", ".join(three_60_d.keys()))
+    print("360: " + ", ".join(three_60_d.keys()))
+    print("Costa Verde Villages: " + ", ".join(cv_d.keys()))
     print("--------------------------------------")
     time.sleep(60)
 
